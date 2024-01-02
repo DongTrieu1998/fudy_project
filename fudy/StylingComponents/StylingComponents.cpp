599c8b4fa8bc7a2357b7aae8be05d97409e6467f
DongTrieu1998 <buinguyendongtrieu2701@gmail.com>
2024-01-03 00:48:48 +0700

    Remove Plugin files, move add Plugin to tech debt

--- fudy/StylingComponents/StylingComponents.cpp
+++
@@ -1,9 +0,0 @@
-#include "myplugin.h"
-#include "mycustomitem.h"
-
-#include <QtQml/QtQml>
-
-void MyPlugin::registerTypes(const char *uri)
-{
-    qmlRegisterType<MyCustomItem>(uri, 1, 0, "MyItem");
-}
